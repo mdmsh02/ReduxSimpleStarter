6720bf5e47beace986bccce01ffff10982c8c532
Saif-Husain <Saif-Hussain@epam.com>
2017-05-27 11:20:53 +0530

    modification: SearchBar as functional component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar'
+
+const API_KEY = 'AIzaSyAliTbSM5LkceWKu4Jm8KpHfvqBXvDQKOE';
 
 const App = () => {
-    return <div>HI</div>
+    return (
+        <div>
+            <SearchBar />
+        </div>
+        )
 }
 ReactDOM.render(<App />,document.querySelector('.container'));
