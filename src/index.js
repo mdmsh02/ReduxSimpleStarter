1cfd3a4897c6912727a1c59f4027ca4b0689a651
Saif-Husain <Saif-Hussain@epam.com>
2017-05-27 12:19:58 +0530

    refactoring: AppComponent to Stateful Component

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
-import React from 'react';
+import React,{ Component} from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar'
-
 const API_KEY = 'AIzaSyAliTbSM5LkceWKu4Jm8KpHfvqBXvDQKOE';
 
-const App = () => {
-    return (
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { videos: []};
+        YTSearch({ key: API_KEY}, videos => this.setState( { videos }));
+    }
+    render() {
         <div>
             <SearchBar />
         </div>
-    )
+    }
 }
 ReactDOM.render(<App />, document.querySelector('.container'));
