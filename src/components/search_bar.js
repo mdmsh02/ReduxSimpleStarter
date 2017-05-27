bc30de2d1f7c5754f4637eb5ed60c1dd9a90fb4e
Saif-Husain <Saif-Hussain@epam.com>
2017-05-27 11:49:16 +0530

    modification: added initial state and modifed the onChange event to update the state

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component{
+    constructor(props){
+        super(props);
+        this.state = { term: ''};
+
+    }
     render() {
-        return <input onChange ={this.onInputChange} />;
+        return (
+            <div>
+                <input onChange ={ event => this.setState({ term: event.target.value})} />
+            </div>
+        );
     }
 
-    onInputChange(event) {
-        console.log(event.target.value);
-
-    }
+    
 }
 
 export default SearchBar
