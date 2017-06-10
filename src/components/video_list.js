2316394daff43ca4b52bcfc404a9eba0495204c5
Saif-Husain <Saif-Hussain@epam.com>
2017-06-10 20:42:28 +0530

    addition: callback to select videos

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -3,12 +3,16 @@ import VideoListItem from './video_list_item';
 
 const VideoList = (props) => {
     const VideoItem = props.videos.map((video) => {
-        return <VideoListItem key={video.etag} video={video} />
+        return (
+            <VideoListItem
+                onVideoSelect = {props.onVideoSelect}
+                key={video.etag} video={video} />
+        )
     });
     return (
         <ul className="col-md4 list-group">
             {VideoItem}
-        </ul>    
+        </ul>
     )
 
 }
--- src/index.js
+++ src/index.js
@@ -9,15 +9,23 @@ const API_KEY = 'AIzaSyAliTbSM5LkceWKu4Jm8KpHfvqBXvDQKOE';
 class App extends Component {
     constructor(props) {
         super(props);
-        this.state = { videos: [] };
-        YTSearch({ key: API_KEY, term: 'manchesterUnited' }, videos => this.setState({ videos }));
+        this.state = {
+            videos: [],
+            selectedVideos: null
+        };
+        YTSearch({ key: API_KEY, term: 'manchesterUnited' }, videos => this.setState({
+            videos: videos,
+            selectedVideo: videos[0]
+        }));
     }
     render() {
         return (
             <div>
                 <SearchBar />
-                <VideoDetail video={this.state.videos[0]} />
-                <VideoList videos = { this.state.videos} />
+                <VideoDetail video={this.state.selectedVideo} />
+                <VideoList
+                    onVideoSelect={ (selectedVideo) => { this.setState({selectedVideo})}}
+                    videos={this.state.videos} />
             </div>
         )
     };
