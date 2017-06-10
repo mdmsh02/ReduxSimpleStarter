c175f56d279ab690f40da2c6c44be93c460e3547
Saif-Husain <Saif-Hussain@epam.com>
2017-06-10 21:14:09 +0530

    addition: added search functionality and updated the video list based on search

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ class App extends Component {
     render() {
         return (
             <div>
-                <SearchBar />
+                <SearchBar onSearch = { term => this.videoSearch(term) } />
                 <VideoDetail video={this.state.selectedVideo} />
                 <VideoList
                     onVideoSelect={(selectedVideo) => { this.setState({ selectedVideo }) }}
