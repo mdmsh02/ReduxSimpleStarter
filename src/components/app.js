a7bdc34c5a12e02462a43cda3e0515ce6f97b917
Saif-Husain <Saif-Hussain@epam.com>
2017-06-11 20:48:06 +0530

    addition: booklist containner and hook it up with reactredux using connect method and mapStateToProps

---
+++ src/components/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import BookList from '../containers/book-list';
+
+
+const App = () => {
+    return (
+        <div>
+            <BookList />
+        </div>
+    )
+}
+export default App;
