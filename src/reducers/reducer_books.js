5914d663ea34227ca61c3a03b9d76e1af344181b
Saif-Husain <Saif-Hussain@epam.com>
2017-06-11 11:47:11 +0530

    addition: first reducer BooksReducer

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function() {
+    return [
+        {title: 'the javascript good parts'},
+        {title: ' the react and redux'},
+        {title: ' you do not know javascript'},
+        {title: 'redux saga and redux thunks'},
+        {title: ' redux perf optimization'}
+    ];
+}
