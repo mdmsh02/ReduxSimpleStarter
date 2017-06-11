c8be27f55e36ebb1e960dec6ffd2d839b9647d39
Saif-Husain <Saif-Hussain@epam.com>
2017-06-11 10:52:35 +0530

    redux initial setup

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import SearchBar from './components/search_bar'
 
-const API_KEY = 'AIzaSyAliTbSM5LkceWKu4Jm8KpHfvqBXvDQKOE';
 
 const App = () => {
     return (
         <div>
-            <SearchBar />
+            Redux Starter
         </div>
     )
 }
